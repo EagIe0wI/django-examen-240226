bca82003fe9f8bd8f8939bbc3aafabac132fb8c0
EagIe0wO <anya230704@gmail.com>
2026-02-24 18:43:43 +0300

    добавлена erd-диаграмма

--- shoestore/settings.py
+++ shoestore/settings.py
@@ -75,8 +75,12 @@ WSGI_APPLICATION = 'shoestore.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'django-exem-240226',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
